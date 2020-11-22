082dfc1579116cd9985c989282f416eb7f545e67
Naoki0722 <mattu.nao722@gmail.com>
2020-11-22 09:22:30 +0900

    imageアップロード問題

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -6,11 +6,23 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Storage;
+
 
 class RegisterController extends Controller
 {
     public function post(Request $request)
     {
+        // $request->validate([
+        //     'image' => 'required|file|image|mimes:png,jpeg'
+        // ]);
+        $upload_image = $request->file('fileInfo');
+        dd($upload_image);
+        // if($upload_image) {
+            $image_name = $upload_image->getClientOriginalName();
+            $save = Storage::disk('s3')->putFileAs('/user', $upload_image, $image_name, 'public');
+            $path= Storage::disk('s3')->url($save);
+        // }
         $now = Carbon::now();
         $hashed_password = Hash::make($request->password);
         $param = [
@@ -21,6 +33,7 @@ class RegisterController extends Controller
             'password' => $hashed_password,
             'account' => $request->account,
             'introducer' => $request->introducer,
+            'image_path' => $path,
             'created_at' => $now,
             'updated_at' => $now
 
