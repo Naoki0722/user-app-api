99e4a9f6d9d416a2ce68ad63a02978f28d7828ab
Naoki0722 <mattu.nao722@gmail.com>
2020-11-25 07:21:50 +0900

    20201125時点でのコード

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Carbon\Carbon;
+use Illuminate\Support\Str;
+use finfo;
 use Illuminate\Support\Facades\Storage;
 
 
@@ -13,16 +15,25 @@ class RegisterController extends Controller
 {
     public function post(Request $request)
     {
-        // $request->validate([
-        //     'image' => 'required|file|image|mimes:png,jpeg'
-        // ]);
-        $upload_image = $request->file('fileInfo');
-        dd($upload_image);
-        // if($upload_image) {
-            $image_name = $upload_image->getClientOriginalName();
-            $save = Storage::disk('s3')->putFileAs('/user', $upload_image, $image_name, 'public');
-            $path= Storage::disk('s3')->url($save);
-        // }
+        $file_name = $request->img;
+        $file_name = preg_replace('/^data:image.*base64,/', '', $file_name);
+        $file_name = str_replace('', '+', $file_name);
+        $image = base64_decode($file_name);
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $mime_type = finfo_buffer( $finfo, $image);
+        $extensions = [
+            'image/gif' => 'gif',
+            'image/jpeg' => 'jpeg',
+            'image/png' => 'png',
+        ];
+        // 保存新ファイル名(ランダム生成)
+        $random_str = Str::random(10);
+        $filename = $random_str . '.' . $extensions[$mime_type];
+
+        $store_dir = 'user';
+        $storefile = sprintf('%s/%s', $store_dir, $filename);
+        Storage::disk('s3')->put($storefile, $image);
+        $image_path = Storage::disk('s3')->url($storefile);
         $now = Carbon::now();
         $hashed_password = Hash::make($request->password);
         $param = [
@@ -33,7 +44,7 @@ class RegisterController extends Controller
             'password' => $hashed_password,
             'account' => $request->account,
             'introducer' => $request->introducer,
-            'image_path' => $path,
+            'image_path' => $image_path,
             'created_at' => $now,
             'updated_at' => $now
 
